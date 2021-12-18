4b0f7620c3c1af46494d27452da978d762ae8095
Shrikant Karve <shrikant.karve@gmail.com>
2021-12-18 21:23:41 +0530

    Includes creating sequences across a range of numbers. Have some performance improvements too.

--- collatz-conjecture.py
+++ collatz-conjecture.py
@@ -4,23 +4,54 @@
 # If a number is odd, multiply it by 3 and add 1
 # If number if even, divide by 2
 
+import argparse
+import pprint
 import sys
-sequence = []
 
-if len(sys.argv) != 2:
-    print("Provide a number to print sequence of")
-    exit(-1)
-num = int(sys.argv[1])
-sequence.append(num)
-#print(num)
-while(True):
-    if num == 1:
-        print(",".join(map(str, sequence)))
-        exit(0)
-    if num%2 == 0:
-        num = num//2
-        sequence.append(num)
-    else:
-        num = 3 * num + 1
-        sequence.append(num)
+seq_len = {}
+seq_dict = {}
+highest = {}
 
+def get_seq(num):
+
+    global seq_dict
+    start_num = num
+    sequence = [num]
+    #sequence.append(num)
+    #print(num)
+    while(True):
+        if num == 1:
+            #print(",".join(map(str, sequence)))
+            break
+        elif num%2 == 0:
+            num = num//2
+            #sequence.append(num)
+        else:
+            num = 3 * num + 1
+            #sequence.append(num)
+        if num <= start_num:
+            sequence.extend(seq_dict[num])
+            break
+        else:
+            sequence.append(num)
+    return sequence
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--max_num", required=True, type=int,
+                        help="Provide the max number you want to calculate\
+                             collatx sequence")
+    parser.add_argument("-v", "--verbose", action="store_true")
+    args = parser.parse_args()
+    print(args)
+
+    for i in range(1,args.max_num+1):
+        seq_dict[i] = get_seq(i)
+        seq_len[i] = len(seq_dict[i]) 
+        highest[i] = max([seq_len[j] for j in range(1,i+1)])
+        if args.verbose:
+            print("sequence for ",i,":",",".join(map(str,seq_dict[i])))
+    if args.verbose:
+        for i in sorted(seq_len.keys()):
+            print("{0:6} : {1:4} : {2:4}".format(i, seq_len[i], highest[i]))
+main()
